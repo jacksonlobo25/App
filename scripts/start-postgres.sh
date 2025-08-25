9a1fefc4353292240361420b42d775ecdc2b91e1
Lobo <jlobo@galaxe.com>
2025-08-25 12:54:41 +0530

    Removed scripts

--- scripts/start-postgres.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-PG_MAJOR=$(cat /etc/postgresql/PG_MAJOR)
-CONF=/etc/postgresql/${PG_MAJOR}/main/postgresql.conf
-PORT=${POSTGRES_PORT:-5432}
-exec /usr/lib/postgresql/${PG_MAJOR}/bin/postgres \
-    -D "${PGDATA}" -c config_file="${CONF}" -c "port=${PORT}"
